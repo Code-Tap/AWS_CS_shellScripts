175ca27ed080f1fab14165e6a12f89af4b72732a
Leigh Wilson <code-tap@leighwilson.xyz>
2021-09-23 22:49:03 +0100

    Removed comments added for purely for testing

--- CreateEKScluster.sh
+++ CreateEKScluster.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# [ "$1" != '' ] &&
-# [[ -z "$1" ]]
-
 if [[ "$1" == '' ]]; then
     echo "No cluster-name Argument Provided"
     echo "CreateEKScluster.sh myCluster"
@@ -13,17 +10,17 @@ elif [[ "$1" != '' ]]; then
     if [[ "${arg,,}" == "d" || "${arg,,}" == "delete" ]]
     then
         echo "Attempting to delete ${2}"
-        # eksctl delete cluster --name ${2} --region us-west-2
+        eksctl delete cluster --name ${2} --region us-west-2
         exit 1
     else
         echo "Standin for create Cluster and check cluster health here"
-        # eksctl create cluster \
-        # --name "${arg,,}" \
-        # --region us-west-2 \
-        # --fargate && \
-        # kubectl get svc && \
-        # kubectl get nodes -o wide && \
-        # kubectl get pods --all-namespaces -o wide
+        eksctl create cluster \
+        --name "${arg,,}" \
+        --region us-west-2 \
+        --fargate && \
+        kubectl get svc && \
+        kubectl get nodes -o wide && \
+        kubectl get pods --all-namespaces -o wide
     fi
 fi
 
